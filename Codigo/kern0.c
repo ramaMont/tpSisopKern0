6983299a7898d9714a186d798f7bb89f2031e930
RamaaNet <ramaa196@gmail.com>
2019-09-17 23:59:16 -0300

    Agrego makefile scripts para correr mas rapido y funcion vga_write

--- Codigo/kern0.c
+++ Codigo/kern0.c
@@ -1,4 +1,27 @@
+#define VGABUF ((volatile char *) 0xb8000)
+
+static void vga_write(const char *s, int linea, int color){
+	volatile char *buf = VGABUF;
+	while(*s != 0){
+		*buf++ = *s++;
+		*buf++ = color;
+	}
+}
+
+
 void comienzo(void) {
+	volatile char *buf = VGABUF;
+/*
+	*buf++ = 79;
+	*buf++ = 47;
+	*buf++ = 75;
+	*buf++ = 47;
+*/
+	char *string="HOLA MUNDO";
+
+	vga_write("Hola Mundo", 2, 2);
+
+
     while (1)
         asm("hlt");
 }
